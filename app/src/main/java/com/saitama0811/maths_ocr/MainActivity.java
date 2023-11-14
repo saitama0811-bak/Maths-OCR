ae85e607dbd48c10acd060e7845fe7ebf4ae89cd
saitama0811 <www.Sgoel.007@gmail.com>
2023-11-14 23:17:33 +0530

    text recognize working

--- app/src/main/java/com/saitama0811/maths_ocr/MainActivity.java
+++ app/src/main/java/com/saitama0811/maths_ocr/MainActivity.java
@@ -17,26 +17,36 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.text.Text;
 import com.google.mlkit.vision.text.TextRecognition;
 import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
 import java.io.File;
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
+    private TextView textView;
+    private Button button;
     private String photoPath;
     private int rotationDegree = 0;
     TextRecognizer recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS);
@@ -46,7 +56,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         imageView = (ImageView) findViewById(R.id.displayImage);
+        textView = (TextView) findViewById(R.id.showReadText);
+        button = (Button) findViewById(R.id.openCamera);
         askCameraPermission();
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                launchCamera();
+            }
+        });
     }
 
     private void askCameraPermission() {
@@ -82,10 +99,7 @@ public class MainActivity extends AppCompatActivity {
 
             Bitmap bitmap = BitmapFactory.decodeFile(photoPath);
             imageView.setImageBitmap(bitmap);
-
-            InputImage image = InputImage.fromBitmap(bitmap, rotationDegree);
-
-
+            getTextRecognition(bitmap);
 //            if (data != null) {
 //                Bundle extras = data.getExtras();
 //                if (extras != null) {
@@ -105,9 +119,80 @@ public class MainActivity extends AppCompatActivity {
                 launchCamera();
             }
             else {
-                Toast.makeText(this, CAMERA_PERMISSION_ERROR, Toast.LENGTH_SHORT).show();
+                showToast(CAMERA_PERMISSION_ERROR);
             }
         }
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void getTextRecognition(Bitmap bitmap) {
+        InputImage image = InputImage.fromBitmap(bitmap, rotationDegree);
+        recognizeText(image);
+    }
+
+    private void recognizeText(InputImage image) {
+        Task<Text> result =
+                recognizer.process(image)
+                        .addOnSuccessListener(new OnSuccessListener<Text>() {
+                            @Override
+                            public void onSuccess(Text visionText) {
+                                processTextBlock(visionText);
+//                                showToast(visionText.getText());
+                                for (Text.TextBlock block : visionText.getTextBlocks()) {
+                                    Rect boundingBox = block.getBoundingBox();
+                                    Point[] cornerPoints = block.getCornerPoints();
+                                    String text = block.getText();
+
+                                    for (Text.Line line: block.getLines()) {
+                                        // ...
+                                        for (Text.Element element: line.getElements()) {
+                                            // ...
+                                            for (Text.Symbol symbol: element.getSymbols()) {
+                                                // ...
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                });
+    }
+
+    private void processTextBlock(Text result) {
+        // [START mlkit_process_text_block]
+        String resultText = result.getText();
+        for (Text.TextBlock block : result.getTextBlocks()) {
+            String blockText = block.getText();
+            Point[] blockCornerPoints = block.getCornerPoints();
+            Rect blockFrame = block.getBoundingBox();
+            for (Text.Line line : block.getLines()) {
+                String lineText = line.getText();
+                Point[] lineCornerPoints = line.getCornerPoints();
+                Rect lineFrame = line.getBoundingBox();
+                for (Text.Element element : line.getElements()) {
+                    String elementText = element.getText();
+                    Point[] elementCornerPoints = element.getCornerPoints();
+                    Rect elementFrame = element.getBoundingBox();
+                    for (Text.Symbol symbol : element.getSymbols()) {
+                        String symbolText = symbol.getText();
+                        Point[] symbolCornerPoints = symbol.getCornerPoints();
+                        Rect symbolFrame = symbol.getBoundingBox();
+                    }
+                }
+            }
+        }
+        // [END mlkit_process_text_block]
+        textView.setText(resultText);
+        showToast(resultText);
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
     }
 }
